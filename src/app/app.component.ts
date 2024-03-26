18cd09c9ee060ffcb480749f46e62cea61f9b957
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-03-25 22:36:20 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Shopping List';
+  shoppingList!: Array<Item>
+
+  constructor(private list: ListaDeCompraService) { }
+
+  ngOnInit(): void {
+    this.shoppingList = this.list.getListaDeCompra();
+    console.log(this.shoppingList);
+  }
+
+}
